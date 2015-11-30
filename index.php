493f23a6bd6e2cd11300324c9bcbf190ed64bcbd
divyanshch <dchou002@student.ucr.edu>
2015-11-30 00:08:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -32,24 +32,89 @@
         <div class = "search_box">
             <form id="searchbox" action="index.php">
                 <input name="search" type="text" placeholder="Type here">
-		<input type="radio" name="search_type" value="BM25" checked="checked">BM25
-		<input type="radio" name="search_type" value="TFIDF">TFIDF
-		<input type="radio" name="search_type" value="frequency">Frequency
-		<input id="submit" type="submit" value="Search">
+				<input type="radio" name="search_type" value="BM25" checked="checked"> BM25
+				<input type="radio" name="search_type" value="TFIDF">TFIDF
+				<input type="radio" name="search_type" value="frequency">Frequency
+				<input id="submit" type="submit" value="Search">
             </form>
         </div>
       </div>
     </div> 
 
-	<?php 
-	
-	?>		
+	   <?php
+       if(isset($_GET["search_type"]))
+	   {
+	   		$search_type = $_GET["search_type"];
+			$search = $_GET["search"];
+		   
+
+	if($search_type == "BM25")
+	{
+	?>
+		<div class="BM25">
+		  <div class="container">
+			<h2>Search Results</h2>
+			<p>Below are your result <?php echo $search; ?> ranked by BM25</p>
+				<div class="row">
+				  <div class="col-md-4">
+				    <h3>Top 10 results</h3>
+				  </div>	
+			</div>
+	      </div>
+	    </div>
+	<?php
+	}			
+	else if($search_type=="TFIDF")
+	    {
+	   ?>
+	    <div class="TFIDF">
+	      <div class="container">
+			<h2>Search Results</h2>
+			<p>Below are your result ranked by BM25</p>
+				<div class="row">
+				  <div class="col-md-4">
+				    <h3>Top 3 results</h3>
+				  </div>
+				  <div class="col-md-4">
+				    <h3>Top 6 results</h3>
+				  </div>
+				  <div class="col-md-4">
+			    <h3>Top 6 results</h3>
+			  </div>		
+			</div>
+	      </div>
+	    </div>
+		<?php
+	    }	
+	else if($search_type="frequency")
+	    {
+	   ?>
+	    <div class="Frequency">
+	      <div class="container">
+			<h2>Search Results</h2>
+			<p>Below are your result ranked by BM25</p>
+				<div class="row">
+				  <div class="col-md-4">
+				    <h3>Top 3 results</h3>
+				  </div>
+				  <div class="col-md-4">
+				    <h3>Top 6 results</h3>
+				  </div>
+				  <div class="col-md-4">
+			    <h3>Top 6 results</h3>
+			  </div>		
+			</div>
+	      </div>
+	    </div>
+		<?php
+	    }	
+	}
+       ?>	
     
 	<!---div class="BM25">
         <div class="container">
             <h2>Search Results</h2>
             <p>Below are your results ranked by BM25 through Whoosh.</p>
-
              <div class="row">
                 <div class="col-md-4">
 					<h3>Top 3 results</h3>	
@@ -65,7 +130,6 @@
             </div>
         </div>
     </div>
-
 	<div class="PageRank">
 	  <div class="container">
 		<h2>Search results</h2>
@@ -87,4 +151,3 @@
 	</div-->
   </body>
 </html>
-
