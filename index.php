9fe81bbb53ef2b5da8027bf87366714e3dc250b3
hfanc001 <hfanc001@ucr.edu>
2015-12-10 16:51:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -10,100 +10,71 @@
   </head>
 
   <body>
-    <!--div class="nav">
-      <div class="container">
-        <ul class = "pull-left">
-          <li><a href="#">Name</a></li>
-          <li><a href="#">Browse</a></li>
-        </ul>
-        <ul class = "pull-right">
-          <li><a href="#">Sign Up</a></li>
-          <li><a href="#">Log In</a></li>
-          <li><a href="#">Help</a></li>
-        </ul>
-      </div>
-    </div-->
 
     <div class="jumbotron">
       <div class="container">
-        <h1>Search Engine</h1>
+		<h1><a href="main.php">Search Engine</a></h1>
         <p>Input your keywords in the search box below.</p>
-	<p>You can choose between BM25, TFIDF, Frequency</p>
+	<p>You can choose between BM25, TFIDF</p>
         <div class = "search_box">
-            <form id="searchbox" action="index.php">
-                <input name="search" type="text" placeholder="Type here">
+            <form id="searchbox" action="main.php">
+                <input name="search" type="text" <placeholder="Type here">
 				<input type="radio" name="search_type" value="BM25" checked="checked"> BM25
 				<input type="radio" name="search_type" value="TFIDF">TFIDF
-				<input type="radio" name="search_type" value="frequency">Frequency
-				<input id="submit" type="submit" value="Search">
+				<input id="submit" type="submit" value="Search"><br/>
+				&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp14; &emsp14;
+				<input type="radio" name="operation_type" value="AND" checked="checked"> AND
+				<input type="radio" name="operation_type" value="OR"> OR
+				
             </form>
+
         </div>
       </div>
     </div> 
 
 	   <?php
-       if(isset($_GET["search_type"]))
+	  
+       if(isset($_GET["search_type"]) && isset($_GET["search"]) && isset($_GET["operation_type"]))
 	   {
+	   		
 	   		$search_type = $_GET["search_type"];
+			$operation_type = $_GET["operation_type"];
 			$search = $_GET["search"];
-		   
-
-	if($search_type == "BM25")
+			$check = trim($search," ");
+			
+	if($search_type == "BM25" && !empty($check))
 	{
 	?>
+		
 		<div class="BM25">
 		  <div class="container">
-			<h2>Search Results</h2>
-			<p>Below are your result <?php echo $search; ?> ranked by BM25</p>
-				<div class="row">
-				  <div class="col-md-4">
-				    <h3>Top 10 results</h3>
-				  </div>	
-			</div>
+<?php					// This will contain: array('status' => 'Yes!')
+
+			$result="";
+			$command = 'python C:\xampp\htdocs\searcher.py ';
+			$search = "\"".$search . "\"";
+			$command .= "$search, $search_type, $operation_type";
+			system($command);
+			
+	?>			  
 	      </div>
 	    </div>
 	<?php
 	}			
-	else if($search_type=="TFIDF")
+	else if($search_type=="TFIDF" && !empty($check))
 	    {
 	   ?>
 	    <div class="TFIDF">
-	      <div class="container">
-			<h2>Search Results</h2>
-			<p>Below are your result ranked by BM25</p>
-				<div class="row">
-				  <div class="col-md-4">
-				    <h3>Top 3 results</h3>
-				  </div>
-				  <div class="col-md-4">
-				    <h3>Top 6 results</h3>
-				  </div>
-				  <div class="col-md-4">
-			    <h3>Top 6 results</h3>
-			  </div>		
-			</div>
-	      </div>
-	    </div>
-		<?php
-	    }	
-	else if($search_type="frequency")
-	    {
-	   ?>
-	    <div class="Frequency">
-	      <div class="container">
-			<h2>Search Results</h2>
-			<p>Below are your result ranked by BM25</p>
-				<div class="row">
-				  <div class="col-md-4">
-				    <h3>Top 3 results</h3>
-				  </div>
-				  <div class="col-md-4">
-				    <h3>Top 6 results</h3>
-				  </div>
-				  <div class="col-md-4">
-			    <h3>Top 6 results</h3>
-			  </div>		
-			</div>
+	     <div class="container">
+<?php					// This will contain: array('status' => 'Yes!')
+
+			$result="";
+			$command = 'python C:\xampp\htdocs\searcher.py ';
+			$search = "\"".$search . "\"";
+			$command .= "$search, $search_type, $operation_type";
+			system($command);
+			
+	?>			  
 	      </div>
 	    </div>
 		<?php
@@ -111,6 +82,9 @@
 	}
        ?>	
     
+	
+	
+	
 	<!---div class="BM25">
         <div class="container">
             <h2>Search Results</h2>
